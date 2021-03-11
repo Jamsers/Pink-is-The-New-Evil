5f6332adf0eb54e6d349c7dc20f8f95081879ce7
Jamsers <jjgutib@gmail.com>
2021-03-11 14:54:27 +0800

    fixed motion vector bug for green goo, tweaked post processing

---
+++ Assets/motionvetorfix.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class motionvetorfix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ParticleSystemRenderer renderer = GetComponent<ParticleSystemRenderer>();
+		renderer.motionVectorGenerationMode = MotionVectorGenerationMode.Camera;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
