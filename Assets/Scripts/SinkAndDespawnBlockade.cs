9fe987385a4dd22f0df68b718d7a6c3e564234db
John James Gutib <jjgutib@gmail.com>
2021-04-17 11:48:20 +0800

    improvements to weapons and lights

--- Assets/Scripts/SinkAndDespawnBlockade.cs
+++ Assets/Scripts/SinkAndDespawnBlockade.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class SinkAndDespawnBlockade : MonoBehaviour {
 
-	void Start () {
-		
-	}
+    Vector3 moveUpperCubeorigpos;
+    public float shakeAmount;
+    public float shakeInterval;
+    public Transform objectsContainer;
+
+    void Start () {
+        
+        
+        
+    }
 	
 	void Update () {
         Sink();
+
 	}
+
+    void MoveUpper()
+    {
+        moveUpperCubeorigpos = objectsContainer.localPosition;
+        objectsContainer.localPosition = new Vector3(objectsContainer.localPosition.x + shakeAmount / 2, objectsContainer.localPosition.y + shakeAmount / 2, objectsContainer.localPosition.z + shakeAmount / 2);
+        Invoke("MoveLower", shakeInterval);
+        Invoke("MoveUpper2", shakeInterval*2);
+        Invoke("MoveLower", shakeInterval*3);
+        Invoke("MoveFinish", shakeInterval*4);
+        //Debug.Log("moveup");
+    }
+
+    void MoveLower()
+    {
+        objectsContainer.localPosition = new Vector3(objectsContainer.localPosition.x - shakeAmount, objectsContainer.localPosition.y - shakeAmount, objectsContainer.localPosition.z - shakeAmount);
+        //Debug.Log("movelow");
+    }
+
+    void MoveUpper2()
+    {
+        objectsContainer.localPosition = new Vector3(objectsContainer.localPosition.x + shakeAmount, objectsContainer.localPosition.y + shakeAmount, objectsContainer.localPosition.z + shakeAmount);
+        //Debug.Log("moveup");
+    }
+
+    void MoveFinish()
+    {
+        //cube.position = new Vector3(cube.position.x + .05f, cube.position.y + .05f, cube.position.z + .05f);
+        objectsContainer.localPosition = moveUpperCubeorigpos;
+        Invoke("MoveUpper", shakeInterval);
+        //Debug.Log("moveup");
+    }
 
     public GameObject[] smokes;
 
@@ -31,6 +70,7 @@ public class SinkAndDespawnBlockade : MonoBehaviour {
     void Sink() {
         if (initiateSink == true) {
             if (hasCreatedInitalParams == false) {
+                MoveUpper();
                 GameObject.FindWithTag("Player").GetComponent<PlaySoundEffect>().PlaySound(13);
                 beginPos = transform.position;
                 endPos = new Vector3(transform.position.x, transform.position.y - 5, transform.position.z);
---
+++ Assets/lightfadein.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightfadein : MonoBehaviour {
+
+	Light light;
+	float origBrightness;
+	bool isFading = false;
+	float fadeamount = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light>();
+		origBrightness = light.intensity;
+		light.intensity = 0;
+	}
+
+	void OnEnable()
+    {
+		isFading = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFading)
+        {
+			light.intensity = Mathf.MoveTowards(light.intensity, origBrightness, fadeamount * Time.deltaTime);
+			if (light.intensity >= origBrightness)
+            {
+				isFading = false;
+            }
+		}
+		
+	}
+}
