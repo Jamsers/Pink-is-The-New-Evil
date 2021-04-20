3d99a5c779a647a622a6c8106af0424434483631
John James Gutib <jjgutib@gmail.com>
2021-04-20 21:11:06 +0800

    initial cleanup

--- Assets/Scripts/ChangeVersionWindows.cs
+++ Assets/Scripts/ChangeVersionWindows.cs
@@ -2,31 +2,35 @@
 using UnityEngine;
 using UnityEditor;
 
-public class changeversion : EditorWindow
+public class ChangeVersionWindows : EditorWindow
 {
     string versionNum;
     string lastVersion;
     bool changed;
+
     [MenuItem("Window/Version Helper")]
     public static void ShowWindow()
     {
-        EditorWindow.GetWindow(typeof(changeversion));
+        EditorWindow.GetWindow(typeof(ChangeVersionWindows));
     }
 
     void OnGUI()
     {
-        if (!changed)
+        if (changed == false)
         {
             versionNum = PlayerSettings.bundleVersion;
             lastVersion = versionNum;
         }
+
         EditorGUIUtility.labelWidth = 80;
         versionNum = EditorGUILayout.TextField("Version", versionNum);
+
         if (lastVersion != versionNum)
         {
             changed = true;
         }
-        if (changed)
+
+        if (changed == true)
         {
             if (GUILayout.Button("Set Version"))
             {
