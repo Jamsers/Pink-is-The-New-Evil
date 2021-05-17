a1d579deba18bee97295d462219ea7e68a006087
Jamsers <jjgutib@gmail.com>
2021-05-17 18:14:42 +0800

    further minified loading screen

---
+++ Assets/RotateLoadingCircle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotateLoadingCircle : MonoBehaviour {
+    public RectTransform rectTransform;
+    const float RotationSpeed = 750f;
+    Vector3 currentRotation;
+
+    void Update() {
+        currentRotation = rectTransform.rotation.eulerAngles;
+        currentRotation.z += RotationSpeed * Time.deltaTime;
+        rectTransform.rotation = Quaternion.Euler(currentRotation);
+    }
+}
